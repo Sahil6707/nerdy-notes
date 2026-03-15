bc0a55d3d4f03912fd54a2de0354ed329911f3bb
Sahil6707 <sahilkoslia6707@gmail.com>
2026-03-15 23:31:57 +0530

    clean server configuration for render

--- nerdy-notes-backend/server.js
+++ nerdy-notes-backend/server.js
@@ -1,53 +1,43 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-const authMiddleware = require("./middlewares/authMiddleware");
 const notesRoutes = require("./routes/notesRoutes");
 const purchaseRoutes = require("./routes/purchaseRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
-
-const path = require("path");
 const app = express();
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-
+// Connect database
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Backend is alive");
-});
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/authRoutes"));
+// Static folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", notesRoutes);
 app.use("/api/purchase", purchaseRoutes);
 app.use("/api/admin", adminRoutes);
 
-app.get("/test", (req, res) => {
-  res.send("Server working");
-});
-
+// Test route
 app.get("/", (req, res) => {
   res.send("Nerdy Notes API Running");
 });
 
+app.get("/test", (req, res) => {
+  res.send("Server working");
+});
+
+// Port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-app.get("/protected", authMiddleware, (req, res) => {
-
-  res.json({
-    message: "You accessed a protected route",
-    user: req.user
-  });
-
 });
