e85986a9f7fcd7ee22ac41801833a368dc3b038a
Sahil6707 <sahilkoslia6707@gmail.com>
2026-03-16 00:43:56 +0530

    fix upload folder for render

--- nerdy-notes-backend/middlewares/upload.js
+++ nerdy-notes-backend/middlewares/upload.js
@@ -1,13 +1,20 @@
 const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadPath = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-destination: function (req, file, cb) {
-cb(null, "uploads/");
-},
-
-filename: function (req, file, cb) {
-cb(null, Date.now() + "-" + file.originalname);
-}
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
 const upload = multer({ storage });
