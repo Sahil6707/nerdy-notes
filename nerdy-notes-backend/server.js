90b92ff690df274b10c04e1d716d6217c77b3a8a
Sahil6707 <sahilkoslia6707@gmail.com>
2026-03-15 23:02:50 +0530

    fix render port issue

--- nerdy-notes-backend/server.js
+++ nerdy-notes-backend/server.js
@@ -36,7 +36,9 @@ app.get("/test", (req, res) => {
 app.get("/", (req, res) => {
   res.send("Nerdy Notes API Running");
 });
-const PORT = 5000
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
