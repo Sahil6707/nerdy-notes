e14421a78a9ec1c383c3bad4e9736730c223a009
Sahil6707 <sahilkoslia6707@gmail.com>
2026-03-16 02:30:04 +0530

    fix cloudinary pdf upload

--- nerdy-notes-backend/middlewares/upload.js
+++ nerdy-notes-backend/middlewares/upload.js
@@ -4,10 +4,12 @@ const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: {
-    folder: "nerdy-notes",
-    resource_type: "raw",
-    allowed_formats: ["pdf"]
+  params: async (req, file) => {
+    return {
+      folder: "nerdy-notes",
+      resource_type: "raw",
+      public_id: Date.now() + "-" + file.originalname
+    };
   }
 });
 
