36d37b2960b2a28f60f93dda5a8e37946b6e48a1
Sahil6707 <sahilkoslia6707@gmail.com>
2026-03-16 02:34:31 +0530

    fix pdf format upload

--- nerdy-notes-backend/middlewares/upload.js
+++ nerdy-notes-backend/middlewares/upload.js
@@ -4,13 +4,12 @@ const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: async (req, file) => {
-    return {
-      folder: "nerdy-notes",
-      resource_type: "raw",
-      public_id: Date.now() + "-" + file.originalname
-    };
-  }
+  params: async (req, file) => ({
+    folder: "nerdy-notes",
+    resource_type: "raw",
+    public_id: Date.now() + "-" + file.originalname,
+    format: "pdf"
+  })
 });
 
 const upload = multer({ storage });
