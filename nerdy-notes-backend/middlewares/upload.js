641702024446b5867eb1cb2274f0bb6e955ac91e
Sahil6707 <sahilkoslia6707@gmail.com>
2026-03-16 21:56:57 +0530

    force public delivery for pdf

--- nerdy-notes-backend/middlewares/upload.js
+++ nerdy-notes-backend/middlewares/upload.js
@@ -6,11 +6,12 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: async (req, file) => ({
     folder: "nerdy-notes",
-    resource_type: "auto",
+    resource_type: "raw",
+    type: "upload",
+    access_mode: "public",
     public_id: Date.now() + "-" + file.originalname.replace(/\s+/g, "_"),
   })
 });
-
 const upload = multer({ storage });
 
 module.exports = upload;
