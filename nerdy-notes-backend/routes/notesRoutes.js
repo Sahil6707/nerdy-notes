7e2f8e7d42471dd9078ccea8aa575f1d315e7f4e
Sahil6707 <sahilkoslia6707@gmail.com>
2026-03-17 12:48:06 +0530

    final fix

--- nerdy-notes-backend/routes/notesRoutes.js
+++ nerdy-notes-backend/routes/notesRoutes.js
@@ -23,7 +23,7 @@ router.post(
         return res.status(400).json({ message: "No file uploaded" });
       }
       const { title, subject, year, module, type } = req.body;
-      const fileUrl = req.file.path;
+      const fileUrl = req.file.path + ".pdf";
       const note = new Note({
         title,
         subject,
