c42dc71d1924c1f363fd2c3b77e23da1a71bfb67
Sahil6707 <sahilkoslia6707@gmail.com>
2026-03-17 13:14:27 +0530

    fix storage error

--- nerdy-notes-backend/middlewares/upload.js
+++ nerdy-notes-backend/middlewares/upload.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
 
-const storage = new cloudinaryStorage({
+const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     resource_type: "auto"
